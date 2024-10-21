f032d085b0c624b26e8e560ddf628b9e8356ed6c
shij0-zip <josueal.cass@gmail.com>
2024-10-21 17:04:33 -0600

    first code in repo
    a simple C# console app that shows a "Hello World"

---
+++ 23100144_primer_repo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23100144_primer_repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PRESIONA UN BOTON PARA INICIAR");
+
+            Console.ReadKey();
+
+            Console.WriteLine("Hola Mundo");
+
+            Console.ReadKey();
+
+            Console.WriteLine("FIN DEL PROGRAMA");
+
+            Console.ReadKey();
+        }
+    }
+}
